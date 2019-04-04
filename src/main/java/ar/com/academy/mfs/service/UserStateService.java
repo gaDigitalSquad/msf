453cc1c169a895c289b5ccd7e7124e62016a823c
pabloilich-grupoassa <pilich@grupoassa.com>
2019-04-04 14:41:38 -0300

    Se corrige metodo de emtricas

--- src/main/java/ar/com/academy/mfs/service/UserStateService.java
+++ src/main/java/ar/com/academy/mfs/service/UserStateService.java
@@ -2,6 +2,7 @@ package ar.com.academy.mfs.service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -37,8 +38,33 @@ public class UserStateService {
 		userStates.forEach((u,x) -> {
 			listResumenes.add(new ResumenLicencia(String.valueOf(LocalDate.now().getYear()), x.intValue(),u.toString() ));
 		});
-		return listResumenes;
+		return getMonthsCompleted(listResumenes);
+	}
+
+	private List<ResumenLicencia> getMonthsCompleted(List<ResumenLicencia> listResumenes) {
+		ArrayList<Integer> meses = new ArrayList<>(12) ;
+		meses.add(1);
+		meses.add(2);
+		meses.add(3);
+		meses.add(4);
+		meses.add(5);
+		meses.add(6);
+		meses.add(7);
+		meses.add(8);
+		meses.add(9);
+		meses.add(10);
+		meses.add(11);
+		meses.add(12);
 		
+		for (int mes : meses )
+		{
+			if(!listResumenes.stream().anyMatch(p -> p.getMes().equals(String.valueOf(mes) )));
+			{
+				listResumenes.add(new ResumenLicencia(String.valueOf(LocalDate.now().getYear()),0,String.valueOf(mes)));
+			}
+		}
+	    listResumenes.sort(Comparator.comparing(ResumenLicencia::getAnio));
+	    return listResumenes;
 	}
 	
 }
